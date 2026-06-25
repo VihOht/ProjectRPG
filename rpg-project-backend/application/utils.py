c0a3386d7bca8aff6c3c5305f5c280c66c7f243c
VihOht <robsonsf@al.insper.edu.br>
2026-06-25 05:21:10 -0300

    feat: resender email sender

--- rpg-project-backend/application/utils.py
+++ rpg-project-backend/application/utils.py
@@ -2,6 +2,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+import resend
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
 
@@ -10,6 +11,8 @@ env = Environment(
     loader=FileSystemLoader("application/templates"),
     autoescape=select_autoescape(["html", "xml"])
 )
+
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 
 def render_template(template_name: str, data: dict) -> str:
@@ -28,18 +31,25 @@ def send_email(
 ):
     html_body = render_template(template_name, template_data)
 
-    msg = MIMEMultipart("alternative")
-    msg["From"] = os.getenv("SENDER_EMAIL")
-    msg["To"] = recipient_email
-    msg["Subject"] = subject
+    # msg = MIMEMultipart("alternative")
+    # msg["From"] = os.getenv("SENDER_EMAIL")
+    # msg["To"] = recipient_email
+    # msg["Subject"] = subject
 
-    msg.attach(MIMEText(html_body, "html"))
+    # msg.attach(MIMEText(html_body, "html"))
 
     try:
-        with smtplib.SMTP(os.getenv("SMTP_SERVER"), int(os.getenv("SMTP_PORT")), timeout=10) as server:
-            server.ehlo()
-            server.starttls()
-            server.login(os.getenv("SENDER_EMAIL"), os.getenv("SENDER_PASSWORD"))
-            server.send_message(msg)
+        resend.Emails.send({
+            "from": os.getenv("SENDER_EMAIL"),
+            "to": [recipient_email],
+            "subject": subject,
+            "html": html_body
+            }
+        )
+        # with smtplib.SMTP(os.getenv("SMTP_SERVER"), int(os.getenv("SMTP_PORT")), timeout=10) as server:
+        #     server.ehlo()
+        #     server.starttls()
+        #     server.login(os.getenv("SENDER_EMAIL"), os.getenv("SENDER_PASSWORD"))
+        #     server.send_message(msg)
     except Exception as e:
         return str(e)
